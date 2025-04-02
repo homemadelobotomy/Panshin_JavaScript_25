f31612d33a1323a4de620ffe20416a7a162d205e
homemadelobotomy <panshinmv@gmail.com>
2025-04-02 16:26:17 +0300

    Выполнение второй лабораторной работы

--- calculator_logic.js
+++ calculator_logic.js
@@ -1,26 +1,37 @@
-window.onload = function(){ 
 
+window.onload = function(){ 
+    
     let a = ''
     let b = ''
     let expressionResult = ''
     let selectedOperation = null
+    
     
     // окно вывода результата
     outputElement = document.getElementById("result")
     
     // список объектов кнопок циферблата (id которых начинается с btn_digit_)
     digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+
+    function output(el){
+        let maxLen = 10
+        el = String(el)
+        if (el.length > maxLen ){
+            el = el.slice(0,maxLen) + "..." + el.slice(-4)
+        }
+        outputElement.innerHTML = el
+    }
     
     function onDigitButtonClicked(digit) {
         if (!selectedOperation) {
             if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
                 a += digit
             }
-            outputElement.innerHTML = a
+           output(a)
         } else {
             if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
                 b += digit
-                outputElement.innerHTML = b        
+                output(b)     
             }
         }
     }
@@ -37,22 +48,38 @@ window.onload = function(){
     document.getElementById("btn_op_mult").onclick = function() { 
         if (a === '') return
         selectedOperation = 'x'
+    
     }
     document.getElementById("btn_op_plus").onclick = function() { 
         if (a === '') return
+        if (b != ''){
+            a = (+a) + (+b)
+            b = ''
+            output(a)
+        }
+        
         selectedOperation = '+'
+
     }
     document.getElementById("btn_op_minus").onclick = function() { 
         if (a === '') return
+        if (b != ''){
+            a = (+a) - (+b)
+            b = ''
+            output(a)
+        }
+        
         selectedOperation = '-'
     }
     document.getElementById("btn_op_div").onclick = function() { 
         if (a === '') return
         selectedOperation = '/'
+    
     }
     document.getElementById("btn_op_percent").onclick = function(){
         if (a === '') return
         selectedOperation = '%'
+    
     }    
 
     // кнопка очищения
@@ -64,31 +91,92 @@ window.onload = function(){
         outputElement.innerHTML = 0
     }
 
-    // document.getElementById("btn_op_sign").onclick = function(){
-    //     b = ''
-    //     selectedOperation = ''
-    //     a = -(+outputElement.innerHTML) 
-    //     a.toString()
-    //     outputElement.innerHTML = a
-    //     expressionResult = ''
-    // }
+    document.getElementById("btn_additional_theme").onclick = function(){
+        let element = document.getElementById("result")
+        if (element.classList.contains("light-mode")){
+            element.classList.remove("light-mode")
+            element.classList.add("dark-mode")
+        }
+        else{
+            element.classList.remove("dark-mode")
+            element.classList.add("light-mode")
+        }
+    }
+    document.getElementById("btn_additional_degrees").onclick = function(){
+        if(!selectedOperation){
+            if (a === ''){
+                return
+            }
+            else{
+                a = (+a) * Math.PI / 180
+                output(a)
+            }
+        }
+        else{
+            if (b === ''){
+                return
+            }
+            else{
+                b = (+b) * Math.PI / 180
+                output(a)
+            }
+        }
+    }
 
     document.getElementById("btn_op_sign").onclick = function(){
+        b = ''
+        selectedOperation = ''
+        a = -(+outputElement.innerHTML) 
+        output(a)
+        expressionResult = ''
+    }
+
+    document.getElementById("btn_backspace").onclick = function(){
         if(!selectedOperation){
             if (a.length > 0){
                 a = a.slice(0,-1)
-                outputElement.innerHTML = a
+                outputElement.innerHTML = a.length === 0 ? '0' : a;
             }
-            else{
+        else {
                 if (b.length > 0){
-                    b = b.slice(0,-1)
-                    outputElement.innerHTML = b
+                    b = b.slice(0,-1) 
+                    outputElement.innerHTML = b.length === 0 ? '0' : b
                 }
             }
         }
     }
-    
-    
+    document.getElementById("btn_additional_000").onclick = function(){
+        if(!selectedOperation && a != ''){
+            a = (+a)* 1000
+            output(a)
+        }
+        else if (b != ''){
+            b = (+b) * 1000
+            output(b)
+        }
+    }
+    function factorial(num){
+        if (num < 0){
+            return 
+        }
+        if (num == 0){
+            return 1
+        }
+        else{
+            return(num * factorial(num-1))
+        }
+    }
+    document.getElementById("btn_additional_factorial").onclick = function(){
+        if(!selectedOperation && a != ''){
+            a = factorial(Math.trunc((+a)))
+            output(a)
+        }
+        else if (selectedOperation && b != ''){
+            b = factorial(Math.trunc((+b)))
+            output(b)
+        }
+    }
+
     // кнопка расчёта результата
     document.getElementById("btn_op_equal").onclick = function() { 
         if (a === '' || b === '' || !selectedOperation)
@@ -109,12 +197,16 @@ window.onload = function(){
                 break;
             case '%':
                 expressionResult = (+a) * (+b) / 100
+                break;
         }
-        
-        a = expressionResult.toString()
+        a = expressionResult
+        output(a)
         b = ''
         selectedOperation = null
-    
-        outputElement.innerHTML = a
     }
     };
+
+    function toggleAdditionalOperations(){
+        let operations = document.getElementById("additional_op")
+        operations.style.display = operations.style.display == 'flex' ? 'none' : 'flex' 
+    }
